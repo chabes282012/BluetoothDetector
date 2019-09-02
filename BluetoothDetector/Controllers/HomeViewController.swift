a35e23b2f81dd1f5f8da6b51ad66fa3b93be620e
Donkemezuo <raymonddonkemezuo.org>
2019-09-02 15:35:37 -0400

    Can detect bluetooth devices in nearby vicinity

--- BluetoothDetector/Controllers/HomeViewController.swift
+++ BluetoothDetector/Controllers/HomeViewController.swift
@@ -7,14 +7,47 @@
 //
 
 import UIKit
+import CoreBluetooth
 
 class HomeViewController: UIViewController {
 
+    private var homeView = HomeView()
+    private var nearbyBluetoothDevices = [CBPeripheral]() {
+        didSet {
+            DispatchQueue.main.async {
+                self.homeView.bluetoothTableView.reloadData()
+            }
+        }
+    }
+    private var centralManager = CBCentralManager()
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = UIColor.lightGray
+        view.addSubview(homeView)
+        centralManager.delegate = self
+        print(nearbyBluetoothDevices.count)
+      //  homeView.bluetoothTableView.dataSource = self
+       // homeView.bluetoothTableView.delegate = self
     }
-
-
 }
+
+extension HomeViewController: CBCentralManagerDelegate {
+    func centralManagerDidUpdateState(_ central: CBCentralManager) {
+        if central.state == .poweredOn {
+            centralManager.scanForPeripherals(withServices: nil, options: nil)
+        } else {
+            showAlert(title: "Attention", message: "Please turn on the Bluetooth of your device. Go to Setting ==>> Bluetooth ==>> ON")
+        }
+    }
+    
+    func centralManager(_ central: CBCentralManager, didDiscover peripheral: CBPeripheral, advertisementData: [String : Any], rssi RSSI: NSNumber) {
+        if !nearbyBluetoothDevices.contains(peripheral) {
+            nearbyBluetoothDevices.append(peripheral)
+            print(nearbyBluetoothDevices.count)
+        }
+    }
+}
+
+
+
 
